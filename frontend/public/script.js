6a0d914429cafd3c4dce9f7a10cf8441f39eee65
Your Name <you@example.com>
2023-11-29 12:05:37 +0100

    error handling

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,30 @@
 console.log('loaded')
+const rootElement = document.querySelector("#root")
+
+const fetchUrl = async (url) => {
+  const data = await fetch(url)
+  return data.json()
+
+  // if (data.ok) {
+  //   /* ha a kapott válasz OK status kóddal rendelkezik */
+  //   return data.json()
+  // } else if (data.status === 400 || data.status === 404) {
+  //   return data.json()
+  // } else {
+  //   /* ha a kapott válasz nem OK status kóddal rendelkezik */
+  //   return "bad request"
+  // }
+}
+
+async function init() {
+  const beerData = await fetchUrl("/beers/10")
+  rootElement.innerHTML = JSON.stringify(beerData)
+  
+  /* if (beerData === "bad request") {
+    rootElement.innerHTML = "BAD USER"
+  } else {
+    rootElement.innerHTML = JSON.stringify(beerData)
+  } */
+}
+
+init()
--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const fs = require('fs')
+const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/frontend/index.html'))
 })
 
-/* app.get('/style.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/public/style.css'))
-}) */
-
 app.use('/public', express.static(path.join(__dirname, '/frontend/public')))
 
+app.get('/beers', (req, res) => {
+  res.sendFile(path.join(__dirname, '/data/beers.json'))
+})
+
+app.get('/beers/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+
+  if (isNaN(id)) {
+    res.status(400).json("id must be a number!")
+  } else {
+    fs.readFile(path.join(__dirname, '/data/beers.json'), 'utf8', (err, rawData) => {
+      if (err) {
+        console.log(err)
+        res.status(500).json("file not found")
+      } else {
+        const data = JSON.parse(rawData)
+        const foundBeer = data.find((beer) => beer.id === id)
+        if (foundBeer) res.json(foundBeer)
+        else res.status(404).json("id not exists")
+      }
+    })
+  }
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on http://127.0.0.1:${port}`)
 })
